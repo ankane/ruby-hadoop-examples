7e0e8ea26c6267c0e3104dfd9778faa35e43bf88
Andrew Kane <andrew@getformidable.com>
2011-07-21 00:43:14 -0700

    Fixed reducers

--- taxes/reduce.rb
+++ taxes/reduce.rb
@@ -1,32 +1,17 @@
 #!/usr/bin/env ruby
 
 prev_key = nil
-key_total = 0
+total = 0
 
 ARGF.each do |line|
+   key, value = line.chomp.split("\t")
 
-   # remove any newline
-   line = line.chomp
-
-   # split key and value on tab character
-   (key, value) = line.split(/\t/)
-
-   # check for new key
-   if prev_key && key != prev_key && key_total > 0
-
-      # output total for previous key
-
-      # <key><tab><value><newline>
-      puts prev_key + "\t" + key_total.to_s
-
-      # reset key total for new key
-      prev_key = key
-      key_total = 0
-
-   elsif !prev_key
-      prev_key = key
+   if key != prev_key
+     puts [prev_key, total].join("\t") unless prev_key.nil?
+     total = 0
+     prev_key = key
    end
-
-   # add to count for this current key
-   key_total += value.to_i
+   total += value.to_i
 end
+
+puts [prev_key, total].join("\t") unless prev_key.nil?
